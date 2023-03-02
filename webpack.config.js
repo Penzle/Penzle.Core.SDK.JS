9806c0673690aec4e12dfec5e5f94b9e4a594294
Admir Mujkic <48647732+admir-live@users.noreply.github.com>
2023-03-02 15:14:53 +0100

    feat: add feature  BREAKING CHANGE: The behavior for version
    feat: add feature  BREAKING CHANGE: The behavior for version
    
    Signed-off-by: Admir Mujkic <48647732+admir-live@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin; 
 
 const isProd = (args) => {
 	return args.mode === 'production';
